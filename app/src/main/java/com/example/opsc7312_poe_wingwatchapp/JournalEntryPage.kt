8cc5354d64a6ea5c8956940bd78dc3e3a2a0329a
VixeyPopcorn3 <134959380+VixeyPopcorn3@users.noreply.github.com>
2023-11-12 12:19:45 +0200

    Final part Journal classes

--- app/src/main/java/com/example/opsc7312_poe_wingwatchapp/JournalEntryPage.kt
+++ app/src/main/java/com/example/opsc7312_poe_wingwatchapp/JournalEntryPage.kt
@@ -1,11 +1,88 @@
 package com.example.opsc7312_poe_wingwatchapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.util.*
 
 class JournalEntryPage : AppCompatActivity() {
+    private lateinit var entryTxt: TextView
+
+    private var loginId: Int = 0
+    private val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_journal_entry_page)
+
+        loginId = intent.getIntExtra("loginId", 0)
+
+        entryTxt = findViewById<EditText>(R.id.entryEdittxt)
+
+        val Backbtn = findViewById<Button>(R.id.backbtn)
+
+        Backbtn.setOnClickListener()
+        {
+            val intent = Intent(this@JournalEntryPage, MainPageFrame::class.java)
+
+            if (loginId != null) {
+                val bundle = Bundle()
+                bundle.putInt("loginId", loginId)
+                intent.putExtras(bundle)
+            }
+
+            startActivity(intent)
+            finish()
+        }
+        val Savebtn = findViewById<Button>(R.id.Savebtn)
+
+        Savebtn.setOnClickListener()
+        {
+            val entry = entryTxt.text.toString().trim()
+
+            if (entry.isEmpty()) {
+                Toast.makeText(this, "Please fill out all fields", Toast.LENGTH_SHORT).show()
+            } else {
+                saveEntry(entry, loginId)
+            }
+        }
     }
+    private fun saveEntry(entry: String, loginID: Int) {
+        val date = getCurrentDateAsString()
+        val journal = hashMapOf(
+            "Entry" to entry,
+            "LoginID" to loginID,
+            "Date" to date
+        )
+
+        val journalCollection = db.collection("Journal")
+        journalCollection.add(journal)
+            .addOnSuccessListener { documentReference ->
+                Toast.makeText(this, "Journal Entry added: ${documentReference.id}", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainPageFrame::class.java)
+
+                if (loginId != null) {
+                    val bundle = Bundle()
+                    bundle.putInt("loginId", loginId)
+                    intent.putExtras(bundle)
+                }
+
+                startActivity(intent)
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, "Error adding Journal Entry: $e", Toast.LENGTH_SHORT).show()
+            }
+    }
+    fun getCurrentDateAsString(): String {
+        val dateFormat = SimpleDateFormat("dd-MM-yyyy")
+        val date = Date()
+        return dateFormat.format(date)
+    }
+
 }
--- app/src/main/java/com/example/opsc7312_poe_wingwatchapp/JournalFragment.kt
+++ app/src/main/java/com/example/opsc7312_poe_wingwatchapp/JournalFragment.kt
@@ -1,19 +1,94 @@
 package com.example.opsc7312_poe_wingwatchapp
 
+import android.app.DatePickerDialog
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.util.*
 
 class JournalFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var journalAdapter: JournalAdapter
+    private val journalList: MutableList<Journal> = mutableListOf()
 
+    private lateinit var caltxt: TextView
+
+    private var loginId: Int = 0
+    private val db = Firebase.firestore
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_journal2, container, false)
+        val view = inflater.inflate(R.layout.fragment_journal2, container, false)
+
+
+        loginId = arguments?.getInt("loginId") ?: 0
+
+        recyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        journalAdapter = JournalAdapter(journalList)
+        recyclerView.adapter = journalAdapter
+
+        val newEntrybtn = view.findViewById<Button>(R.id.newEntrybtn)
+        newEntrybtn.setOnClickListener {
+            // Create an Intent to open the NewActivity
+            val intent = Intent(requireContext(), JournalEntryPage::class.java)
+
+            val fragment = JournalFragment()
+            val bundle = Bundle()
+            bundle.putInt("loginId", loginId) // Cast loginId to an integer
+            intent.putExtras(bundle)
+            fragment.arguments = bundle
+
+            startActivity(intent)
+            // Finish the current activity to close it
+            activity?.finish()
+        }
+
+        fetchJournal()
+
+        return view
     }
+
+    private fun fetchJournal() {
+        // Clear the existing journal
+        journalList.clear()
+
+        // Create a Firestore query for observations filtered by loginId
+        var query = db.collection("Journal")
+            .whereEqualTo("LoginID", loginId)
+
+        // Execute the query
+        query.get()
+            .addOnSuccessListener { querySnapshot ->
+                for (document in querySnapshot) {
+                    val entry = document.getString("Entry")
+                    val date = document.getString("Date")
+
+                    if (entry != null && date != null) {
+                        val journal = Journal(entry, date,loginId)
+                        journalList.add(journal)
+                    }
+                }
+
+                // Notify the adapter that the data has changed
+                journalAdapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(context, "Error in loading Journal Entries: $e", Toast.LENGTH_SHORT).show()
+            }
+    }
+
 
 }
